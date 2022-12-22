61e9425ff5b01dcd3bd5511b40b4b96334c03b50
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-12-22 19:17:50 -0300

    dependencies in users module ok

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,16 @@
+import { User, UserSchema } from './schemas/user.schema';
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    ConfigModule,
+    JwtModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UsersService],
   exports: [UsersService],
 })
