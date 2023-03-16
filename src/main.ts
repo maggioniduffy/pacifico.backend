e43c7c7593f5c9172884a7542e3c30e6a1feaeb9
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2023-03-16 19:47:36 -0300

    https

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/pacificoapi.online/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/pacificoapi.online/fullchain.pem',
+    ),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(compression());
-  const port = 5000;
+  const port = process.env.PORT || 5000;
   await app.listen(port);
   logger.log(`Applicatiopn listening on port ${port}`);
 }
